18b4b2cdd0e87603e57e70cc4f805cb76d777f9a
Faridwm <fwmubarok@gmail.com>
2021-07-12 12:56:51 +0700

    1. Mengubah penggunaan webview untuk menampilkan pdf menjadi pdf view. 2. Men-disable dark mode pada app. 3. Menambahkan aset pdf iqro.

--- app/src/main/java/com/fwmubarok/muslimkid/MainActivity.java
+++ app/src/main/java/com/fwmubarok/muslimkid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.fwmubarok.muslimkid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
 import android.content.Intent;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         btn_iqra = findViewById(R.id.btn_act_iqra);
         btn_doa = findViewById(R.id.btn_act_doa);
